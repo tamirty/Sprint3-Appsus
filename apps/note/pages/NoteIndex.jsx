96682f51d00ed5056ae72ea994107b0c306b10f2
DASA2626 <shaidasa541@gmail.com>
2026-08-11 17:32:17 +0300

    add NoteList

--- apps/note/pages/NoteIndex.jsx
+++ apps/note/pages/NoteIndex.jsx
@@ -1,4 +1,28 @@
+const { useState, useEffect } = React
+const { Link } = ReactRouterDOM
+
+
+import { NoteList } from '../cmps/NoteList.jsx'
+import { NotePreview } from '../cmps/NotePreview.jsx'
+import { NoteTxt } from '../cmps/NoteTxt.jsx'
+import { noteService } from '../services/note.service.js'
+
+
+
 
 export function NoteIndex() {
-    return <section className="container">Notes app</section>
+    const [notes, setNotes] = useState([])
+    console.log('notes:', notes)
+
+
+    useEffect(() => {
+        noteService.query().then(setNotes)
+    }, [])
+
+
+    return (
+        <section className="container">
+            <NoteList />
+        </section>
+    )
 }
