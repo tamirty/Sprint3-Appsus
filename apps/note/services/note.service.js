5b6c8e5dfe6ab4f2ed22db6b5a713dfcb06daabf
DASA2626 <shaidasa541@gmail.com>
2026-08-11 17:46:14 +0300

    add service

--- apps/note/services/note.service.js
+++ apps/note/services/note.service.js
@@ -1,1 +1,67 @@
-// note service
+
+
+const NOTE_KEY = 'notesDB'
+// _createNote()
+
+export const noteService = {
+    query,
+    // get,
+    // remove,
+    // save,
+    // getEmptyCar,
+    // getDefaultFilter,
+    // getSpeedStats,
+    // getVendorStats,
+    // getFilterFromSearchParams,
+}
+function query() {
+    return Promise.resolve(notes)
+  }
+ 
+
+
+
+const notes = [
+{
+id: 'n101',
+createdAt: 1112222,
+type: 'NoteTxt',
+isPinned: true,
+style: {
+backgroundColor: '#00d'
+},
+info: {
+txt: 'Fullstack Me Baby!'
+}
+},
+{
+id: 'n102',
+createdAt: 1112223,
+type: 'NoteImg',
+isPinned: false,
+ style: {
+backgroundColor: '#0d0'
+},
+info: {
+url: 'http://some-img/me',
+title: 'Bobi and Me'
+}
+},
+{
+id: 'n103',
+createdAt: 1112224,
+type: 'NoteTodos',
+isPinned: false,
+ style: {
+backgroundColor: '#d00'
+},
+info: {
+title: 'Get my stuff together',
+todos: [
+{ txt: 'Driving license', isDone: true },
+{ txt: 'Coding power', isDone: false }
+]
+}
+}
+]
+
