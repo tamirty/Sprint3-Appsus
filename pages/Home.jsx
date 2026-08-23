dbe43e66f9321ef8ae15627b3cfaa141c42f002a
tamirty <tamirty@gmail.com>
2026-08-23 11:56:38 +0300

    add logos for home and adjust css

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1,12 +1,19 @@
 import { showSuccessMsg } from '../services/event-bus.service.js'
 
+const { Link } = ReactRouterDOM
+
 export function Home() {
     return <section className="container home">
-        <h1>AppSus</h1>
-        <button onClick={() => showSuccessMsg('Yep, that works')}>Show Msg</button>
         <div className="box-container">
-            <div className="box1"></div>
-            <div className="box2"></div>
+            <Link className="app-card" to="/mail">
+                <img className="app-logo" src="assets/img/appsus-mail-logo.png" alt="Appsus Mail" />
+            </Link>            
+            <Link className="app-card" to="/note">
+                <img className="app-logo" src="assets/img/appsus-note-logo.png" alt="Appsus Mail" />
+            </Link>            
+            <Link className="app-card" to="/book">
+                <img className="app-logo-book" src="assets/img/appsus-book-logo.png" alt="Appsus Mail" />
+            </Link>            
         </div>
     </section>
 }
